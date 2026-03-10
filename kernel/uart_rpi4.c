00accfe553908a36c7eb1fc5f1e5ee3c6f52f764
embedclub <scc.ddcx@gmail.com>
2026-03-10 15:08:10 +0000

    suit rpi4 2

--- kernel/uart_rpi4.c
+++ kernel/uart_rpi4.c
@@ -53,7 +53,7 @@ void
 uartinit(void)
 {
   // disable interrupts.
-
+  
   // special mode to set baud rate.
 
   // LSB for baud rate of 38.4K.
@@ -66,6 +66,47 @@ uartinit(void)
   // reset and enable FIFOs.
 
   // enable transmit and receive interrupts.
+	unsigned int selector;
+
+	/* clean gpio14 */
+	selector = readl(GPFSEL1);
+	selector &= ~(7<<12);
+	/* set alt0 for gpio14 */
+	selector |= 4<<12;
+	/* clean gpio15 */
+	selector &= ~(7<<15);
+	/* set alt0 for gpio15 */
+	selector |= 4<<15;
+	writel(selector, GPFSEL1);
+
+	selector = readl(GPIO_PUP_PDN_CNTRL_REG0);
+	selector |= (0x2 << 30) | (0x2 << 28);
+	writel(selector, GPIO_PUP_PDN_CNTRL_REG0);	
+	/* disable UART until configuration is done */
+	writel(0, U_CR_REG);
+
+	/*
+	 * baud divisor = UARTCLK / (16 * baud_rate)
+	= 48 * 10^6 / (16 * 115200) = 26.0416666667
+	integer part = 26
+	fractional part = (int) ((0.0416666667 * 64) + 0.5) = 3
+	generated baud rate divisor = 26 + (3 / 64) = 26.046875
+	generated baud rate = (48 * 10^6) / (16 * 26.046875) = 115177
+	error = |(115177 - 115200) / 115200 * 100| = 0.02%
+	*/
+
+	/* baud rate divisor, integer part */
+	writel(26, U_IBRD_REG);
+	/* baud rate divisor, fractional part */
+	writel(3, U_FBRD_REG);
+
+	/* enable FIFOs and 8 bits frames */
+	writel((1<<4) | (3<<5), U_LCRH_REG);
+
+	/* mask interupts */
+	writel(0, U_IMSC_REG);
+	/* enable UART, receive and transmit */
+	writel(1 | (1<<8) | (1<<9), U_CR_REG);
   initlock(&uart_tx_lock, "uart");
 }
 
